d456ddf9d67c42f0d002bf181dc03bf5b68ee69c
artivis <TO_FILL>
2016-04-01 23:33:15 +0200

    basically renaming & changing typedef

--- src/pointcloud_to_rangeimage_node.cpp
+++ src/pointcloud_to_rangeimage_node.cpp
@@ -24,12 +24,13 @@ namespace
 {
   typedef pcl::PointXYZ              PointType;
   typedef pcl::PointCloud<PointType> PointCloud;
+  typedef PointCloud::ConstPtr       PointCloudConstPtr;
 
-  typedef pcl::RangeImage          RI;
-  typedef pcl::RangeImageSpherical RIS;
+  typedef pcl::RangeImage               RangeImage;
+  typedef boost::shared_ptr<RangeImage> RangeImagePtr;
 
-  typedef pointcloud_to_rangeimage::PointCloudToRangeImageReconfigureConfig conf;
-  typedef dynamic_reconfigure::Server<conf>               RangeImageReconfServer;
+  typedef pointcloud_to_rangeimage::PointCloudToRangeImageReconfigureConfig Conf;
+  typedef dynamic_reconfigure::Server<Conf>               RangeImageReconfServer;
 
   typedef image_transport::ImageTransport It;
   typedef image_transport::Publisher      Pub;
@@ -53,19 +54,20 @@ private:
 
   boost::mutex _mut;
 
-  cv::Mat _rangeImage;
-  PointCloud _pointcloud;
+  cv::Mat            _rangeImage;
+  PointCloudConstPtr _pointcloud_ptr;
 
-  boost::shared_ptr<RIS> rangeImageSph_;
+  RangeImagePtr _range_image_ptr;
+
   RangeImageHeader _header;
 
-  ros::NodeHandle nh_;
+  ros::NodeHandle _nh;
 
-  It              it_;
-  Pub             pub_;
-  ros::Subscriber sub_;
+  It              _it;
+  Pub             _pub;
+  ros::Subscriber _sub;
 
-  boost::shared_ptr<RangeImageReconfServer> drsv_;
+  boost::shared_ptr<RangeImageReconfServer> _drsv;
 
 public:
 
@@ -73,17 +75,16 @@ public:
     _newmsg(false),
     _rgb_range_img(false),
     _laser_frame(true),
-    nh_("~"),
-    it_(nh_)
+    _range_image_ptr(new RangeImage),
+    _nh("~"),
+    _it(_nh)
   {
-    rangeImageSph_ = boost::shared_ptr<RIS>(new RIS);
-
-    drsv_.reset(new RangeImageReconfServer(ros::NodeHandle("pointcloud_to_rangeimage_dynreconf")));
+    _drsv.reset(new RangeImageReconfServer(ros::NodeHandle("pointcloud_to_rangeimage_dynreconf")));
 
     RangeImageReconfServer::CallbackType cb;
     cb = boost::bind(&RangeImageConverter::drcb, this, _1, _2);
 
-    drsv_->setCallback(cb);
+    _drsv->setCallback(cb);
 
     double ang_res_x, ang_res_y,
            max_ang_w, max_ang_h,
@@ -108,18 +109,20 @@ public:
     _header.ang_res_x = static_cast<float>(ang_res_x);
     _header.ang_res_y = static_cast<float>(ang_res_y);
 
-    pub_ = it_.advertise("image_out", 1);
     _header.max_ang_w = static_cast<float>(max_ang_w);
     _header.max_ang_h = static_cast<float>(max_ang_h);
 
-    ros::NodeHandle nh;
-    sub_ = nh.subscribe<PointCloud>("point_cloud_in", 1, &RangeImageConverter::callback, this);
     _header.min_range = static_cast<float>(min_range);
     _header.max_range = static_cast<float>(max_range);
 
     _header.laser_frame = _laser_frame;
 
     _frame = (_laser_frame)? pcl::RangeImage::LASER_FRAME : pcl::RangeImage::CAMERA_FRAME;
+
+    _pub = _it.advertise("image_out", 1);
+
+    ros::NodeHandle nh;
+    _sub = nh.subscribe<PointCloud>("point_cloud_in", 1, &RangeImageConverter::callback, this);
   }
 
   ~RangeImageConverter()
@@ -133,7 +136,7 @@ public:
 
     boost::mutex::scoped_lock(_mut);
 
-    _pointcloud = *msg;
+    _pointcloud_ptr = msg;
 
     _newmsg = true;
   }
@@ -141,7 +144,7 @@ public:
   void convert()
   {
     // What the point if nobody cares ?
-    if (pub_.getNumSubscribers() <= 0)
+    if (_pub.getNumSubscribers() <= 0)
       return;
 
     if (!_newmsg)
@@ -149,16 +152,16 @@ public:
 
     boost::mutex::scoped_lock(_mut);
 
-    _range_image_ptr->createFromPointCloud(_pointcloud,
+    _range_image_ptr->createFromPointCloud(*_pointcloud_ptr,
                                          pcl::deg2rad(_header.ang_res_x), pcl::deg2rad(_header.ang_res_y),
                                          pcl::deg2rad(_header.max_ang_w), pcl::deg2rad(_header.max_ang_h),
                                          I, _frame, 0.0, 0.0f, 0);
 
-    rangeImageSph_->header.frame_id = _pointcloud.header.frame_id;
-    rangeImageSph_->header.stamp    = _pointcloud.header.stamp;
+    _range_image_ptr->header.frame_id = _pointcloud_ptr->header.frame_id;
+    _range_image_ptr->header.stamp    = _pointcloud_ptr->header.stamp;
 
-    int cols = rangeImageSph_->width;
-    int rows = rangeImageSph_->height;
+    int cols = _range_image_ptr->width;
+    int rows = _range_image_ptr->height;
 
     sensor_msgs::ImagePtr msg;
 
@@ -183,7 +186,7 @@ public:
       for (int i=0; i<cols; ++i)
         for (int j=0; j<rows; ++j)
         {
-          pcl::PointWithRange p = rangeImageSph_->getPoint(i, j);
+          pcl::PointWithRange p = _range_image_ptr->getPoint(i, j);
 
           float range = std::max(0.0f, std::min(1.0f, factor * (p.range + offset)));
 
@@ -205,7 +208,7 @@ public:
       {
         for (int j=0; j<rows; ++j)
         {
-          float r = rangeImageSph_->getPoint(i, j).range;
+          float r = _range_image_ptr->getPoint(i, j).range;
 
           float range = (!std::isinf(r))?
                 std::max(0.0f, std::min(1.0f, factor * (r + offset))) :
@@ -224,9 +227,8 @@ public:
 
     msg = cv_bridge::CvImage(std_msgs::Header(), encoding, _rangeImage).toImageMsg();
 
-    pcl_conversions::fromPCL(rangeImageSph_->header, msg->header);
+    pcl_conversions::fromPCL(_range_image_ptr->header, msg->header);
 
-    pub_.publish(msg);
     RangeImageMsg range_image_msg;
 
     range_image_msg.Specifics.laser_frame = static_cast<int>(_frame);
@@ -236,17 +238,22 @@ public:
     range_image_msg.Specifics.min_range = min_range;
     range_image_msg.Specifics.max_range = max_range;
 
-    range_image_msg.Specifics.ang_res_x = rangeImageSph_->getImageOffsetX();
-    range_image_msg.Specifics.ang_res_x = rangeImageSph_->getImageOffsetY();
+    range_image_msg.Specifics.ang_res_x = _range_image_ptr->getImageOffsetX();
+    range_image_msg.Specifics.ang_res_x = _range_image_ptr->getImageOffsetY();
 
     range_image_msg.Image = *msg;
+
+    _pub.publish(msg);
+
+    // I need an image_transport_plugin to do that, crap!
+    //_pub.publish(range_image_msg);
 
     _newmsg = false;
   }
 
 private:
 
-  void drcb(conf &config, uint32_t level)
+  void drcb(Conf &config, uint32_t level)
   {
     _header.ang_res_x = config.ang_res_x;
     _header.ang_res_y = config.ang_res_y;
